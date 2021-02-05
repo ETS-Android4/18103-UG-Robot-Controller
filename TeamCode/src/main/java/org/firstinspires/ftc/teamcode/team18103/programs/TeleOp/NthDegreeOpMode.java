3a97d3ea3393559477596483bc0c0918e1747153
Strker74 <Strker74@gmail.com>
2021-02-05 16:14:10 -0500

    [Update] Driver Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team18103/programs/TeleOp/NthDegreeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team18103/programs/TeleOp/NthDegreeOpMode.java
@@ -37,7 +37,7 @@ public class NthDegreeOpMode extends OpMode {
 //        telemetry.addData("Horizontal: ", robot.getDriveSubsystem().backRight.getCurrentPosition());
 //        telemetry.addData("Chosen One: ", robot.getDriveSubsystem().backLeft.getCurrentPosition());
 
-        robot.getDriveSubsystem().POVMecanumDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, DriveMode.Balanced);
+        robot.getDriveSubsystem().POVMecanumDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, DriveMode.Sport);
 
         robot.getIOSubsystem().runIntake(gamepad1.right_trigger - gamepad1.left_trigger);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team18103/programs/TeleOp/TestRobotAutoAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team18103/programs/TeleOp/TestRobotAutoAim.java
@@ -54,14 +54,14 @@ public class TestRobotAutoAim extends OpMode {
                 gamepad1.left_bumper, gamepad1.right_bumper, gamepad1.x);*/
 
         robot.getDriveSubsystem().POVMecanumDrive(gamepad1.left_stick_y, gamepad1.left_stick_x,
-                gamepad1.right_stick_x, DriveMode.Balanced); // Half Speed
+                gamepad1.right_stick_x, DriveMode.Sport); // Max Speed
 
         robot.getDriveSubsystem().zeroCoords(gamepad1.a);
 
         robot.getIOSubsystem().runIntake(gamepad1.right_trigger - gamepad1.left_trigger);
 
         if(gamepad1.y) {
-            robot.getDriveSubsystem().rotateToShootingAngle();
+            //robot.getDriveSubsystem().rotateToShootingAngle();
             /*telemetry.addData("targetAngle", targetAngle);
             double dist = Math.abs(targetAngle - robot.getDriveSubsystem().getDataFusionTheta());
 
