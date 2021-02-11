2992a315cfd31b6af05e701d035ec488f65129a1
Strker74 <Strker74@gmail.com>
2021-02-11 17:14:06 -0500

    Update Drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team18103/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team18103/subsystems/Drive.java
@@ -261,7 +261,7 @@ public class Drive extends Subsystem {
         //double targetTheta = Math.toDegrees(Math.atan2(Constants.Gx-getDataFusionX(),
         //        Constants.Gy-getDataFusionY())) - 10;
 
-        double targetTheta = Math.toDegrees(Math.atan((Constants.Gx-getDataFusionX())/(Constants.Gy-getDataFusionY()))) - 10;
+        double targetTheta = -Math.toDegrees(Math.atan((Constants.Gx-getDataFusionX())/(Constants.Gy-getDataFusionY()))) - 10;
 
         CustomDriveRotate(targetTheta, 15);
 
